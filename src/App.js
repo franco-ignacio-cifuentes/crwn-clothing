2ca25eaa65e1f129f417022f4c59600e6fe2d957
Franco Cifuentes <franco.cifuentes@norcal.aaa.com>
2022-12-23 10:58:45 -0300

    77. Adding Sass

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-const App = () => {
+import CategoryItem from './components/category-item/category-item.component';
 
+import './categories.styles.scss';
+
+const App = () => {
   const categories = [
     {
       id: 1,
@@ -25,17 +28,11 @@ const App = () => {
 
   return (
     <div className='categories-container'>
-      {categories.map(({ title }) => (
-        <div className='category-container'>
-          <div className='background-image' />
-          <div className='category-body-container'>
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
       ))}
     </div>
-  )
-}
+  );
+};
 
 export default App;

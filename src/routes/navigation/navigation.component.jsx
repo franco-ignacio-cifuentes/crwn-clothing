88651a688810d5df82537838fc5ac453f0f99c97
Franco Cifuentes <franco.cifuentes@norcal.aaa.com>
2022-12-28 15:52:45 -0300

    106. Re-rendering From Context

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -8,8 +8,7 @@ import './navigation.styles.scss';
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
-  console.log(currentUser);
-  
+
   return (
     <Fragment>
       <div className='navigation'>
